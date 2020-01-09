b49e160232e486d0ab2cd3fd16a9222d3a440271
WHysson <41398337+WHysson@users.noreply.github.com>
2020-01-09 13:22:11 +0200

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormComponent } from './form/form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule, 
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  dateFrom: null;
+  dateTo:null;
+  startDate: "ВЫБЕРИТЕ ДАТУ НАЧАЛА"
+
+ 
+  constructor() {
+   
+   }
+
+   
+
+  ngOnInit() {
+    
+  }
+
+    
+  
+
+  
+
+}
